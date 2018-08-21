6c9ec07a72a32181c07d586faab60fdc539c3c54
ZeroPointTwo <zeropointtwo@gmail.com>
2018-08-21 16:31:24 -0400

    WIP: Started defining components.

--- src/Component.h
+++ src/Component.h
@@ -3,14 +3,41 @@
 
 namespace Breakout
 {
-  class Component
+  class BaseComponent
   {
     public:
-    Component();
-    virtual ~Component() = default;
+    BaseComponent();
+    virtual ~BaseComponent() = 0;
+
+    virtual bool Init()           = 0;
+    virtual void Update(float dt) = 0;
+    virtual void UnInit()         = 0;
 
     private:
   };
+
+  class RenderComponent : public BaseComponent
+  {
+    public:
+    BaseComponent();
+    virtual ~BaseComponent() = 0;
+
+    virtual bool Init()           = 0;
+    virtual void Update(float dt) = 0;
+    virtual void UnInit()         = 0;
+  };
+
+  class PositionComponent : public BaseComponent
+  {
+    public:
+    BaseComponent();
+    virtual ~BaseComponent() = 0;
+
+    virtual bool Init()           = 0;
+    virtual void Update(float dt) = 0;
+    virtual void UnInit()         = 0;
+  };
+
 }  // namespace Breakout
 
 #endif  // !BREAKOUT_COMPONENT_H
