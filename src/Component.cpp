cf77219bd9569105b4c26e45d82bd0329265d0f1
ZeroPointTwo <zeropointtwo@gmail.com>
2018-08-28 16:21:50 -0400

    WIP: End of day checkin

--- src/Component.cpp
+++ src/Component.cpp
@@ -1,5 +1,38 @@
 #include "Component.h"
 
-using Breakout::Component;
+using Breakout::BaseComponent;
 
-Component::Component() {}
+Breakout::RenderComponent::RenderComponent(const std::shared_ptr<sf::Shape>& inRenderObject) :
+    RenderObject(inRenderObject)
+{
+}
+
+Breakout::RenderComponent::~RenderComponent() {}
+
+bool Breakout::RenderComponent::Init() { return true; }
+
+void Breakout::RenderComponent::Update(float dt)
+{
+    // todo:  DRAW ME and cry
+    //                    -Joel
+}
+
+void Breakout::RenderComponent::UnInit() {}
+
+//-----------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------
+//-------------------------------------
+
+Breakout::PositionComponent::PositionComponent() {}
+
+Breakout::PositionComponent::~PositionComponent() {}
+
+bool Breakout::PositionComponent::Init() { return true; }
+
+void Breakout::PositionComponent::Update(float dt) {}
+
+void Breakout::PositionComponent::UnInit() {}
+
+void Breakout::PositionComponent::SetPosition(const sf::Vector2f& newPosition) { Position = newPosition; }
+
+const sf::Vector2f& Breakout::PositionComponent::GetPosition() const { return Position; }
--- src/Component.h
+++ src/Component.h
@@ -1,42 +1,59 @@
 #ifndef BREAKOUT_COMPONENT_H
 #define BREAKOUT_COMPONENT_H
 
+#include <memory>
+#include "SFML/System/Vector2.hpp"
+
+namespace sf
+{
+    class Shape;
+}
+
 namespace Breakout
 {
-  class BaseComponent
-  {
-    public:
-    BaseComponent();
-    virtual ~BaseComponent() = 0;
+    class BaseComponent
+    {
+      public:
+        BaseComponent();
+        virtual ~BaseComponent() = 0;
 
-    virtual bool Init()           = 0;
-    virtual void Update(float dt) = 0;
-    virtual void UnInit()         = 0;
+        virtual bool Init()           = 0;
+        virtual void Update(float dt) = 0;
+        virtual void UnInit()         = 0;
 
-    private:
-  };
+      private:
+    };
 
-  class RenderComponent : public BaseComponent
-  {
-    public:
-    BaseComponent();
-    virtual ~BaseComponent() = 0;
+    class RenderComponent : public BaseComponent
+    {
+      public:
+        RenderComponent(const std::shared_ptr<sf::Shape>& inRenderObject);
+        virtual ~RenderComponent();
 
-    virtual bool Init()           = 0;
-    virtual void Update(float dt) = 0;
-    virtual void UnInit()         = 0;
-  };
+        virtual bool Init() override;
+        virtual void Update(float dt) override;
+        virtual void UnInit() override;
 
-  class PositionComponent : public BaseComponent
-  {
-    public:
-    BaseComponent();
-    virtual ~BaseComponent() = 0;
+      private:
+        std::shared_ptr<sf::Shape> RenderObject;
+    };
 
-    virtual bool Init()           = 0;
-    virtual void Update(float dt) = 0;
-    virtual void UnInit()         = 0;
-  };
+    class PositionComponent : public BaseComponent
+    {
+      public:
+        PositionComponent();
+        virtual ~PositionComponent();
+
+        virtual bool Init() override;
+        virtual void Update(float dt) override;
+        virtual void UnInit() override;
+
+        void                SetPosition(const sf::Vector2f& newPosition);
+        const sf::Vector2f& GetPosition() const;
+
+      private:
+        sf::Vector2f Position;
+    };
 
 }  // namespace Breakout
 
