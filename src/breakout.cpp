9c6b8a867d965377c24b471aeb4d1c1a2d33c109
ZeroPointTwo <zeropointtwo@gmail.com>
2018-10-04 16:20:59 -0400

    Refactored skeleton app to be broken up into application and game classes.

--- src/breakout.cpp
+++ src/breakout.cpp
@@ -6,91 +6,92 @@
 #include "Util.h"
 
 // screen dimension constants
-const int         SCREEN_WIDTH  = 640;
-const int         SCREEN_HEIGHT = 480;
-const char*       WINDOW_TITLE  = "Breakout";
-sf::RenderWindow* MainWindow    = nullptr;
+const int   SCREEN_WIDTH  = 640;
+const int   SCREEN_HEIGHT = 480;
+const char* WINDOW_TITLE  = "Breakout";
 
 class IApplication
 {
   public:
     virtual ~IApplication() {}
     virtual bool Init()     = 0;
-    virtual void Update()   = 0;
+    virtual void Run()      = 0;
     virtual void Shutdown() = 0;
 };
 
 class GameApplication : public IApplication
 {
   public:
-    GameApplication();
-    ~GameApplication() {}
+    GameApplication(std::shared_ptr<Breakout::Game> inGame);
+    virtual ~GameApplication() {}
 
     // Create window and game
     bool Init() override;
 
-    void Update() override;
+    void Run() override;
     void Shutdown() override;
 
   private:
+    std::shared_ptr<Breakout::Engine> engine;
     std::shared_ptr<Breakout::Game>   game;
     std::shared_ptr<sf::RenderWindow> mainWindow;
 };
-
-// initialize sfml
-bool init()
-{
-    // result of initialization
-    bool success = true;
-
-    Message("Print {}\n", 10);
-    for (auto i = 0; i < 5; ++i) { Assert(true, "Print {}\n", 10); }
-
-    MainWindow = new sf::RenderWindow(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), sf::String(WINDOW_TITLE));
-
-    // @! TODO START HERE - FIXME PLZ
-    auto _Engine(Breakout::Engine(MainWindow));
-
-    return success;
-}
-
-void Quit()
-{
-    if (MainWindow != nullptr)
-    {
-        delete MainWindow;
-        MainWindow = nullptr;
-    }
-}
 
 int main(int argc, char* argv[])
 {
     UNUSED_ARGS(argc, argv);
 
-    if (!init()) { fmt::print("Failed to initialize!\n"); }
-    else
+    // THat other way
+    // std::shared_ptr<Breakout::BreakoutGame> game = std::make_shared<Breakout::BreakoutGame>();
+    // GameApplication                         appTwo(game);
+
+    // Move constructor
+    GameApplication app(std::make_shared<Breakout::BreakoutGame>());
+
+    if (!app.Init())
     {
-        sf::CircleShape testCircle(1.f);  // Create circle of radius 1
-        testCircle.setScale(
-            sf::Vector2f(static_cast<float>(SCREEN_WIDTH) * 0.5f,
-                         static_cast<float>(SCREEN_HEIGHT) * 0.5f));  // Scale the circle to make an ellipses
-        testCircle.setFillColor(sf::Color::Green);
-
-        while (MainWindow->isOpen())
-        {
-            sf::Event sfEvent = {};
-            while (MainWindow->pollEvent(sfEvent))
-            {
-                if (sfEvent.type == sf::Event::Closed) { MainWindow->close(); }
-            }
-
-            MainWindow->clear();  // Remove all drawn content from window
-            MainWindow->draw(testCircle);
-            MainWindow->display();  // Present the window
-        }
+        fmt::print("Failed to initialize!\n");
+        return -1;
     }
 
-    Quit();
+    app.Run();
+    app.Shutdown();
 
     return 0;
 }
+
+GameApplication::GameApplication(std::shared_ptr<Breakout::Game> inGame) : game(inGame) {}
+
+bool GameApplication::Init()
+{
+    mainWindow = std::shared_ptr<sf::RenderWindow>(
+        new sf::RenderWindow(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), sf::String(WINDOW_TITLE)));
+
+    // engine = std::shared_ptr<Breakout::Engine>(new Breakout::Engine(mainWindow));
+
+    return true;
+}
+
+void GameApplication::Run()
+{
+    sf::CircleShape testCircle(1.f);  // Create circle of radius 1
+    testCircle.setScale(
+        sf::Vector2f(static_cast<float>(SCREEN_WIDTH) * 0.5f,
+                     static_cast<float>(SCREEN_HEIGHT) * 0.5f));  // Scale the circle to make an ellipses
+    testCircle.setFillColor(sf::Color::Green);
+
+    while (mainWindow->isOpen())
+    {
+        sf::Event sfEvent = {};
+        while (mainWindow->pollEvent(sfEvent))
+        {
+            if (sfEvent.type == sf::Event::Closed) { mainWindow->close(); }
+        }
+
+        mainWindow->clear();  // Remove all drawn content from window
+        mainWindow->draw(testCircle);
+        mainWindow->display();  // Present the window
+    }
+}
+
+void GameApplication::Shutdown() {}
